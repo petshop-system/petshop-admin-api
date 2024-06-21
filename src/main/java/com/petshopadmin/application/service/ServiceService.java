5c33b11fc09a2e752fd7ddb430efe1eb957405db
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-06-21 00:50:27 -0300

    Implementar parte das validações no método create de ServiceService

--- src/main/java/com/petshopadmin/application/service/ServiceService.java
+++ src/main/java/com/petshopadmin/application/service/ServiceService.java
@@ -1,5 +1,6 @@
 package com.petshopadmin.application.service;
 
+import com.petshopadmin.adapter.output.repository.database.ServiceDatabase;
 import com.petshopadmin.application.domain.ServiceDomain;
 import com.petshopadmin.application.port.input.ServiceUserCase;
 import com.petshopadmin.application.port.output.database.ServiceRepositoryDatabase;
@@ -7,6 +8,8 @@ import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
 import org.apache.commons.lang3.ObjectUtils;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -15,6 +18,11 @@ public class ServiceService implements ServiceUserCase {
 
     static String SERVICE_NOT_FOUND = "service not found";
     static String SERVICE_INTERNAL_SERVER_ERROR = "service internal error";
+    static String ILLEGAL_ARGUMENT_NAME_EXCEPTION = "name cannot be null or empty";
+    static String ILLEGAL_ARGUMENT_PRICE_EXCEPTION = "service price cannot be null or negative";
+    static String ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION = "Description cannot be null or empty";
+    static String ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION = "Description exceeds maximum length of 255 characters";
+
 
     private final ServiceRepositoryDatabase serviceRepositoryDatabase;
 
@@ -51,4 +59,29 @@ public class ServiceService implements ServiceUserCase {
 
         return list;
     }
+
+    @Override
+    public ServiceDomain create(ServiceDomain serviceDomain) throws NotFoundException,InternalServerErrorException {
+        if (ObjectUtils.isEmpty(serviceDomain)) {
+            throw new InternalServerErrorException(SERVICE_INTERNAL_SERVER_ERROR);
+        }
+        if (Objects.isNull(serviceDomain.getName()) || serviceDomain.getName().trim().isEmpty()) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_NAME_EXCEPTION);
+        }
+        if (serviceDomain.getPrice() == null || ObjectUtils.isEmpty(serviceDomain.getPrice()) || serviceDomain.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_PRICE_EXCEPTION);
+        }
+        if (!serviceDomain.isActive() ) {
+            throw  new NotFoundException(SERVICE_NOT_FOUND);
+        }
+        if (Objects.isNull(serviceDomain.getDescription()) || serviceDomain.getDescription().trim().isEmpty()) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION);
+        }
+        if (serviceDomain.getDescription().length() > 255) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION);
+        }
+
+        return serviceRepositoryDatabase.save(serviceDomain);
+    }
+
 }
