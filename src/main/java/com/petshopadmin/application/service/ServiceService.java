7219fa2ae6cd95da53a8cebf3b1fbaff0550d603
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-06-24 18:17:23 -0300

    Adicionar ObjectUtils nas validações e criar metodo de validações separado

--- src/main/java/com/petshopadmin/application/service/ServiceService.java
+++ src/main/java/com/petshopadmin/application/service/ServiceService.java
@@ -20,10 +20,11 @@ public class ServiceService implements ServiceUserCase {
     static String SERVICE_NOT_FOUND = "service not found";
     static String SERVICE_INTERNAL_SERVER_ERROR = "service internal error";
     static String ILLEGAL_ARGUMENT_NAME_EXCEPTION = "name cannot be null or empty";
+    static String ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION = "exceeds maximum length of 255 characters";
     static String ILLEGAL_ARGUMENT_PRICE_EXCEPTION = "service price cannot be null or negative";
-    static String ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION = "Description cannot be null or empty";
-    static String ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION = "Description exceeds maximum length of 255 characters";
-
+    static String ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION = "description cannot be null or empty";
+    static String ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION = "description exceeds maximum length of 255 characters";
+    static String ILLEGAL_ARGUMENT_CONTRACT_EXCEPTION = "Contract ID cannot be null or empty";
 
     private final ServiceRepositoryDatabase serviceRepositoryDatabase;
 
@@ -64,33 +65,36 @@ public class ServiceService implements ServiceUserCase {
     @Override
     @Transactional
     public ServiceDomain create(ServiceDomain serviceDomain) throws NotFoundException,InternalServerErrorException {
+        validateServiceDomain(serviceDomain);
+
+        return serviceRepositoryDatabase.save(serviceDomain);
+    }
+
+    private void validateServiceDomain(ServiceDomain serviceDomain) throws InternalServerErrorException, IllegalArgumentException, NotFoundException {
         if (ObjectUtils.isEmpty(serviceDomain)) {
             throw new InternalServerErrorException(SERVICE_INTERNAL_SERVER_ERROR);
         }
-        if (Objects.isNull(serviceDomain.getName()) || serviceDomain.getName().trim().isEmpty()) {
+        if (ObjectUtils.isEmpty(serviceDomain.getName().trim())) {
             throw new IllegalArgumentException(ILLEGAL_ARGUMENT_NAME_EXCEPTION);
         }
-        if (serviceDomain.getPrice() == null || ObjectUtils.isEmpty(serviceDomain.getPrice()) || serviceDomain.getPrice().compareTo(BigDecimal.ZERO) < 0) {
+        if (serviceDomain.getName().length() > 255) {
+            throw new IllegalArgumentException(ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION);
+        }
+        if (ObjectUtils.isEmpty(serviceDomain.getPrice()) || serviceDomain.getPrice().compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException(ILLEGAL_ARGUMENT_PRICE_EXCEPTION);
         }
-        if (!serviceDomain.isActive() ) {
-            throw  new NotFoundException(SERVICE_NOT_FOUND);
+        if (!serviceDomain.isActive()) {
+            throw new NotFoundException(SERVICE_NOT_FOUND);
         }
-        if (Objects.isNull(serviceDomain.getDescription()) || serviceDomain.getDescription().trim().isEmpty()) {
+        if (ObjectUtils.isEmpty(serviceDomain.getDescription().trim())) {
             throw new IllegalArgumentException(ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION);
         }
         if (serviceDomain.getDescription().length() > 255) {
             throw new IllegalArgumentException(ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION);
         }
-        if (Objects.isNull(serviceDomain.getContract().getId())) {
-            throw new InternalServerErrorException("Contract ID cannot be null");
+        if (ObjectUtils.isEmpty(serviceDomain.getContract()) || ObjectUtils.isEmpty(serviceDomain.getContract().getId())) {
+            throw new InternalServerErrorException(ILLEGAL_ARGUMENT_CONTRACT_EXCEPTION);
         }
-
-
-
-
-
-        return serviceRepositoryDatabase.save(serviceDomain);
     }
 
 }
