959696fd2e893b7771963cf229b9ed2347fdb6a3
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-06-24 19:05:59 -0300

    Adicionar metodo toServiceDatabase na interface ServiceConverterMapper

--- src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
+++ src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
@@ -1,9 +1,11 @@
 package com.petshopadmin.utils.converter;
 
 import com.petshopadmin.adapter.input.http.ServiceRequestHTTP;
+import com.petshopadmin.adapter.output.repository.database.ServiceDatabase;
 import com.petshopadmin.application.domain.ServiceDomain;
 
 public interface ServiceConverterMapper {
 
     ServiceDomain toServiceDomain(ServiceRequestHTTP source);
+    ServiceDatabase toServiceDatabase(ServiceDomain source);
 }
