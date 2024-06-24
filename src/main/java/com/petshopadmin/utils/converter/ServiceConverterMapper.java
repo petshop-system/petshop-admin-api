8612bfda48460d9766dd3b39b65fa7f15ceeeb4f
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-06-24 18:13:23 -0300

    Criar package Utils para implementar Converter Mapper

--- src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
+++ src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
@@ -1,2 +1,9 @@
-package com.petshopadmin.utils.converter;public interface ServiceConverterMapper {
+package com.petshopadmin.utils.converter;
+
+import com.petshopadmin.adapter.input.http.ServiceRequestHTTP;
+import com.petshopadmin.application.domain.ServiceDomain;
+
+public interface ServiceConverterMapper {
+
+    ServiceDomain toServiceDomain(ServiceRequestHTTP source);
 }
--- src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapperImpl.java
+++ src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapperImpl.java
@@ -1,2 +1,26 @@
-package com.petshopadmin.utils.converter;public class ServiceConverterMapperImpl {
+package com.petshopadmin.utils.converter;
+
+import com.petshopadmin.adapter.input.http.ServiceRequestHTTP;
+import com.petshopadmin.application.domain.ContractDomain;
+import com.petshopadmin.application.domain.ServiceDomain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceConverterMapperImpl implements ServiceConverterMapper{
+
+    @Override
+    public ServiceDomain toServiceDomain(ServiceRequestHTTP source) {
+        ServiceDomain domain = new ServiceDomain();
+
+        domain.setName(source.name());
+        domain.setDescription(source.description());
+        domain.setPrice(source.price());
+        domain.setActive(source.active());
+
+        ContractDomain contract = new ContractDomain();
+        contract.setId(source.contractId());
+
+        domain.setContract(contract);
+        return domain;
+    }
 }
