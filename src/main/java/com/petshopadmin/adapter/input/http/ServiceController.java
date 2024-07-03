fe8f7970fc5bf58da5214b9daa272e24656ce5ee
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-03 18:47:48 -0300

    feature: Implementar a validacao criar serviço ao endpoint

--- src/main/java/com/petshopadmin/adapter/input/http/ServiceController.java
+++ src/main/java/com/petshopadmin/adapter/input/http/ServiceController.java
@@ -4,10 +4,12 @@ import com.petshopadmin.adapter.output.repository.database.ServiceDatabase;
 import com.petshopadmin.application.domain.ContractDomain;
 import com.petshopadmin.application.domain.ServiceDomain;
 import com.petshopadmin.application.port.input.ServiceUserCase;
+import com.petshopadmin.application.port.input.ServiceValidation;
 import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
 import com.petshopadmin.utils.converter.ServiceConverterMapper;
 import jakarta.websocket.server.PathParam;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,10 +26,12 @@ public class ServiceController {
 
     private final ServiceUserCase serviceUserCase;
     private final ServiceConverterMapper converterMapper;
+    private final ServiceValidation serviceValidation;
 
-    public ServiceController (ServiceUserCase serviceUserCase, ServiceConverterMapper converterMapper) {
+    public ServiceController (ServiceUserCase serviceUserCase, ServiceConverterMapper converterMapper, ServiceValidation serviceValidation) {
         this.serviceUserCase = serviceUserCase;
         this.converterMapper = converterMapper;
+        this.serviceValidation = serviceValidation;
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -57,12 +61,24 @@ public class ServiceController {
 
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseHTTP create(@RequestBody ServiceRequestHTTP serviceRequestHTTP) throws NotFoundException, InternalServerErrorException, IllegalArgumentException {
         ServiceDomain serviceDomain = converterMapper.toServiceDomain(serviceRequestHTTP);
-        ServiceDomain created = serviceUserCase.create(serviceDomain);
 
-        return new ResponseHTTP("sucess to create a new services", new ServiceResponseHTTP(created), null, LocalDateTime.now());
+        try {
+            serviceValidation.validateServiceDomain(serviceDomain);
+
+            ServiceDomain created = serviceUserCase.create(serviceDomain);
+            return new ResponseHTTP("sucess to create a new services", new ServiceResponseHTTP(created), null, LocalDateTime.now());
+        } catch (IllegalArgumentException e) {
+            return new ResponseHTTP(e.getMessage(), HttpStatus.BAD_REQUEST, null, LocalDateTime.now());
+        }  catch (InternalServerErrorException e) {
+            return new ResponseHTTP(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR , null, LocalDateTime.now());
+        } catch (NotFoundException e) {
+            return new ResponseHTTP(e.getMessage(), HttpStatus.NOT_FOUND , null, LocalDateTime.now());
+        }
+
+
     }
 
 }
---
+++ src/main/java/com/petshopadmin/application/port/input/ServiceValidation.java
@@ -0,0 +1,9 @@
+package com.petshopadmin.application.port.input;
+
+import com.petshopadmin.application.domain.ServiceDomain;
+import com.petshopadmin.exception.InternalServerErrorException;
+import com.petshopadmin.exception.NotFoundException;
+
+public interface ValidationService {
+    void validateServiceDomain (ServiceDomain serviceDomain) throws InternalServerErrorException, IllegalArgumentException, NotFoundException;
+}
