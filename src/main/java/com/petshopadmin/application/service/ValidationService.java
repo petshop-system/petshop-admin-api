74e28507f8de112e7d598ec2c56b0911f8af6693
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-04 22:17:45 -0300

    Implementar lista de erros ao invés de erros individuais

--- src/main/java/com/petshopadmin/application/service/ValidationService.java
+++ src/main/java/com/petshopadmin/application/service/ValidationService.java
@@ -1,10 +1,9 @@
 package com.petshopadmin.application.service;
 
 import com.petshopadmin.application.domain.ServiceDomain;
-import com.petshopadmin.application.port.input.ServiceValidation;
+import com.petshopadmin.application.domain.ValidationDomain;
 import com.petshopadmin.application.port.input.ServiceValidation;
 import com.petshopadmin.exception.InternalServerErrorException;
-import com.petshopadmin.exception.NotFoundException;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.math.BigDecimal;
@@ -23,39 +22,36 @@ public class ValidationService implements ServiceValidation {
     static String ILLEGAL_ARGUMENT_CONTRACT_EXCEPTION = "Contract ID cannot be null or empty";
 
     @Override
-    public void validateServiceDomain(ServiceDomain serviceDomain) throws InternalServerErrorException, IllegalArgumentException, NotFoundException {
-        List<String> errors = new ArrayList<>();
+    public void validateServiceDomain(ServiceDomain serviceDomain) throws InternalServerErrorException, IllegalArgumentException {
+        List<ValidationDomain> errors = new ArrayList<>();
 
         if (ObjectUtils.isEmpty(serviceDomain)) {
             throw new InternalServerErrorException(SERVICE_INTERNAL_SERVER_ERROR);
         }
         if (ObjectUtils.isEmpty(serviceDomain.getName().trim())) {
-            errors.add(ILLEGAL_ARGUMENT_NAME_EXCEPTION);
+            errors.add(new ValidationDomain("name", ILLEGAL_ARGUMENT_NAME_EXCEPTION));
         }
         if (serviceDomain.getName().length() > 255) {
-            errors.add(ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION);
+            errors.add(new ValidationDomain("name", ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION));
         }
         if (ObjectUtils.isEmpty(serviceDomain.getPrice()) || serviceDomain.getPrice().compareTo(BigDecimal.ZERO) < 0) {
-            errors.add(ILLEGAL_ARGUMENT_PRICE_EXCEPTION);
+            errors.add(new ValidationDomain("price", ILLEGAL_ARGUMENT_PRICE_EXCEPTION));
         }
         if (!serviceDomain.isActive()) {
-            errors.add(SERVICE_NOT_FOUND);
+            errors.add(new ValidationDomain("Service active", SERVICE_NOT_FOUND));
         }
         if (ObjectUtils.isEmpty(serviceDomain.getDescription().trim())) {
-            errors.add(ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION);
+            errors.add(new ValidationDomain("description", ILLEGAL_ARGUMENT_DESCRIPTION_EXCEPTION));
         }
         if (serviceDomain.getDescription().length() > 255) {
-            errors.add(ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION);
+            errors.add(new ValidationDomain("description", ILLEGAL_ARGUMENT_CHARACTERS_MAX_DESCRIPTION_EXCEPTION));
         }
-        if (ObjectUtils.isEmpty(serviceDomain.getContract().getId())) {
-            errors.add(ILLEGAL_ARGUMENT_CONTRACT_EXCEPTION);
-        }
-        if (ObjectUtils.isEmpty(serviceDomain.getContract())) {
-            errors.add("contrato inexistente");
+        if (ObjectUtils.isEmpty(serviceDomain.getContract()) || ObjectUtils.isEmpty(serviceDomain.getContract().getId())) {
+            errors.add(new ValidationDomain("contract", ILLEGAL_ARGUMENT_CONTRACT_EXCEPTION));
         }
 
         if (!errors.isEmpty()) {
-            throw new IllegalArgumentException(String.join(", ", errors));
+            throw new IllegalArgumentException(errors.toString());
         }
     }
 }
