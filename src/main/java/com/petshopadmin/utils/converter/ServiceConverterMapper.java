74ab9754aa837f92d220311c8d4baf64d1f30db0
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-04 22:11:21 -0300

    Mapear contract pelo Mapping

--- src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
+++ src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
@@ -1,11 +1,20 @@
 package com.petshopadmin.utils.converter;
 
 import com.petshopadmin.adapter.input.http.ServiceRequestHTTP;
+import com.petshopadmin.adapter.output.repository.database.ContractDatabase;
 import com.petshopadmin.adapter.output.repository.database.ServiceDatabase;
+import com.petshopadmin.application.domain.ContractDomain;
 import com.petshopadmin.application.domain.ServiceDomain;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper
 public interface ServiceConverterMapper {
 
+    @Mapping(target = "contract.id", source = "contractid")
     ServiceDomain toServiceDomain(ServiceRequestHTTP source);
+
+    @Mapping(target = "ID", source = "contract.id")
     ServiceDatabase toServiceDatabase(ServiceDomain source);
+    
 }
