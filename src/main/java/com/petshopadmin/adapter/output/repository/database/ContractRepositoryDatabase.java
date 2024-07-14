f0c793aa185e7e19d6abbc07f5929f51f95a57be
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-14 05:38:23 -0300

    Criar ContractRepositoryDatabase para buscar o contrato por ID

--- src/main/java/com/petshopadmin/adapter/output/repository/database/ContractRepositoryDatabase.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/ContractRepositoryDatabase.java
@@ -1,2 +1,27 @@
-package com.petshopadmin.adapter.output.repository.database;public class ContractRepositoryDatabase {
+package com.petshopadmin.adapter.output.repository.database;
+
+import com.petshopadmin.application.domain.ContractDomain;
+import com.petshopadmin.exception.NotFoundException;
+import com.petshopadmin.utils.converter.ServiceConverterMapper;
+import org.apache.commons.lang3.ObjectUtils;
+
+public class ContractRepositoryDatabase implements com.petshopadmin.application.port.output.database.ContractRepositoryDatabase {
+    public final ContractJPARepository contractJPARepository;
+    public final ServiceConverterMapper serviceConverterMapper;
+
+    public ContractRepositoryDatabase (ContractJPARepository contractJPARepository, ServiceConverterMapper serviceConverterMapper) {
+        this.contractJPARepository = contractJPARepository;
+        this.serviceConverterMapper = serviceConverterMapper;
+    }
+
+    @Override
+    public ContractDomain getById(Long id){
+
+        ContractDatabase contractDatabase = contractJPARepository.getById(id);
+
+        if (ObjectUtils.isEmpty(contractDatabase)) {
+            return null;
+        }
+        return contractDatabase.toContractDomain();
+    }
 }
