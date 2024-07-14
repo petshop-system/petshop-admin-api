7dca8db0af235668e9a5020dcf47cbbf9e41a57e
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-14 05:33:57 -0300

    Refatorar Controller Busca por contrato dentro do Controller

--- src/main/java/com/petshopadmin/adapter/input/http/ServiceController.java
+++ src/main/java/com/petshopadmin/adapter/input/http/ServiceController.java
@@ -3,7 +3,7 @@ package com.petshopadmin.adapter.input.http;
 import com.petshopadmin.application.domain.ContractDomain;
 import com.petshopadmin.application.domain.ServiceDomain;
 import com.petshopadmin.application.port.input.ServiceUserCase;
-import com.petshopadmin.application.port.input.ServiceValidation;
+import com.petshopadmin.application.port.output.database.ContractRepositoryDatabase;
 import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
 import com.petshopadmin.utils.converter.ServiceConverterMapper;
@@ -21,12 +21,12 @@ public class ServiceController {
 
     private final ServiceUserCase serviceUserCase;
     private final ServiceConverterMapper serviceConverterMapper;
-    private final ServiceValidation serviceValidation;
+    private final ContractRepositoryDatabase contractRepositoryDatabase;
 
-    public ServiceController (ServiceUserCase serviceUserCase, ServiceConverterMapper converterMapper, ServiceValidation serviceValidation) {
+    public ServiceController (ServiceUserCase serviceUserCase, ServiceConverterMapper converterMapper, ContractRepositoryDatabase contractRepositoryDatabase) {
         this.serviceUserCase = serviceUserCase;
         this.serviceConverterMapper = converterMapper;
-        this.serviceValidation = serviceValidation;
+        this.contractRepositoryDatabase = contractRepositoryDatabase;
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -59,9 +59,14 @@ public class ServiceController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseHTTP create(@RequestBody ServiceRequestHTTP serviceRequestHTTP) throws NotFoundException, InternalServerErrorException, IllegalArgumentException {
         try {
-            ServiceDomain serviceDomain = serviceConverterMapper.toServiceDomain(serviceRequestHTTP);
 
-            serviceValidation.validateServiceDomain(serviceDomain);
+            Long contractId = serviceRequestHTTP.contractid();
+            ContractDomain contractDomain = contractRepositoryDatabase.getById(contractId);
+            
+            ServiceDomain serviceDomain = serviceConverterMapper.toServiceDomain(serviceRequestHTTP);
+            serviceDomain.setContract(contractDomain);
+
+            serviceUserCase.validate(serviceDomain);
 
             ServiceDomain created = serviceUserCase.create(serviceDomain);
             return new ResponseHTTP("sucess to create a new services", new ServiceResponseHTTP(created), null, LocalDateTime.now());
@@ -70,20 +75,7 @@ public class ServiceController {
         }
     }
 
-    @PostMapping("/validate")
-    public ResponseHTTP validate(@RequestBody ServiceRequestHTTP serviceRequestHTTP) {
-        ServiceDomain serviceDomain = serviceConverterMapper.toServiceDomain(serviceRequestHTTP);
-        try {
-            serviceValidation.validateServiceDomain(serviceDomain);
-            ServiceDomain created = serviceUserCase.create(serviceDomain);
-            return new ResponseHTTP("success to create a new services", new ServiceResponseHTTP(created), null, LocalDateTime.now());
-        } catch (IllegalArgumentException e) {
-            return new ResponseHTTP("Validation errors", e.getCause(), null, LocalDateTime.now());
-        } catch (InternalServerErrorException e) {
-            return new ResponseHTTP(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, null, LocalDateTime.now());
-        } catch (NotFoundException e) {
-            return new ResponseHTTP(e.getMessage(), HttpStatus.NOT_FOUND, null, LocalDateTime.now());
-        }
-    }
+
+
 
 }
