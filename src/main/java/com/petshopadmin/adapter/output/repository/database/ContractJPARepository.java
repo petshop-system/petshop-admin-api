b72c49dcd2984ebb0cd9062b0ccaed79bb81cc59
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-14 05:37:38 -0300

    Criar repositorio de contrato

--- src/main/java/com/petshopadmin/adapter/output/repository/database/ContractJPARepository.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/ContractJPARepository.java
@@ -3,4 +3,6 @@ package com.petshopadmin.adapter.output.repository.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractJPARepository extends JpaRepository<ContractDatabase, Long> {
+
+
 }
