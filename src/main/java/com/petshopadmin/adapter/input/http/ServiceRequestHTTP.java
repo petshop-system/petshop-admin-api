4015fdc7de26cdc2c389d9c4818136c4a11af8d5
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-20 23:44:25 -0300

    style: Resolve indentation

--- src/main/java/com/petshopadmin/adapter/input/http/ServiceRequestHTTP.java
+++ src/main/java/com/petshopadmin/adapter/input/http/ServiceRequestHTTP.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-public record ServiceRequestHTTP(String name,
+
+public record ServiceRequestHTTP(
+                                 String name,
                                  BigDecimal price,
                                  boolean active,
                                  String description,
