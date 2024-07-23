810f1938b42d1a384b2dcc6913f546a8dc5f8205
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-23 01:17:27 -0300

    Refactor: Remove optional to handle the object type

--- src/main/java/com/petshopadmin/adapter/output/repository/database/ContractRepositoryDatabase.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/ContractRepositoryDatabase.java
@@ -3,23 +3,21 @@ package com.petshopadmin.adapter.output.repository.database;
 import com.petshopadmin.application.domain.ContractDomain;
 import org.apache.commons.lang3.ObjectUtils;
 
-import java.util.Optional;
-
 public class ContractRepositoryDatabase implements com.petshopadmin.application.port.output.database.ContractRepositoryDatabase {
     public final ContractJPARepository contractJPARepository;
 
-    public ContractRepositoryDatabase (ContractJPARepository contractJPARepository) {
+    public ContractRepositoryDatabase(ContractJPARepository contractJPARepository) {
         this.contractJPARepository = contractJPARepository;
     }
 
     @Override
     public ContractDomain getById(Long id) {
-        Optional<ContractDatabase> contract = contractJPARepository.findById(id);
+        ContractDatabase contract = contractJPARepository.getByID(id);
 
         if (ObjectUtils.isEmpty(contract)) {
             return null;
         }
 
-        return contract.get().toContractDomain();
+        return contract.toContractDomain();
     }
 }
