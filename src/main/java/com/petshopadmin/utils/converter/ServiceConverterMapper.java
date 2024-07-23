224be6644f450655d5b4f6ff3f847332433fff30
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-23 01:43:32 -0300

    refactor: Remove unnecessary junk

--- src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
+++ src/main/java/com/petshopadmin/utils/converter/ServiceConverterMapper.java
@@ -1,9 +1,6 @@
 package com.petshopadmin.utils.converter;
 
 import com.petshopadmin.adapter.input.http.ServiceRequestHTTP;
-import com.petshopadmin.adapter.output.repository.database.ContractDatabase;
-import com.petshopadmin.adapter.output.repository.database.ServiceDatabase;
-import com.petshopadmin.application.domain.ContractDomain;
 import com.petshopadmin.application.domain.ServiceDomain;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,12 +10,5 @@ public interface ServiceConverterMapper {
 
     @Mapping(target = "contract.id", source = "contractid")
     ServiceDomain toServiceDomain(ServiceRequestHTTP source);
-
-    @Mapping(target = "ID", source = "contract.id")
-    ServiceDatabase toServiceDatabase(ServiceDomain source);
-
-    ContractDomain toContractDomain(Long id);
-
-    @Mapping(target = "ID", source = "id")
-    ContractDatabase toContractDatabase(ContractDomain source);
 }
+
