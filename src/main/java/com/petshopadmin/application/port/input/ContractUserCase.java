c9d5d7a07793f26d82cb931ce519ff71e264591e
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-23 01:30:43 -0300

    style: Resolve indentation

--- src/main/java/com/petshopadmin/application/port/input/ContractUserCase.java
+++ src/main/java/com/petshopadmin/application/port/input/ContractUserCase.java
@@ -1,2 +1,9 @@
-package com.petshopadmin.application.port.input;public class ContractUserCase {
+package com.petshopadmin.application.port.input;
+
+import com.petshopadmin.application.domain.ContractDomain;
+import com.petshopadmin.exception.InternalServerErrorException;
+import com.petshopadmin.exception.NotFoundException;
+
+public interface ContractUserCase {
+    ContractDomain getById(Long id) throws NotFoundException, InternalServerErrorException;
 }
