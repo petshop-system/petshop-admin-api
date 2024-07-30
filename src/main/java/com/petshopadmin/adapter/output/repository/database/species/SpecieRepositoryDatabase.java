c9f1025279aef029fad6d0e1b46dc13bf9c7b691
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-29 23:33:01 -0300

    feat: implement SpecieRepositoryDatabase in adapter layer

--- src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieRepositoryDatabase.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieRepositoryDatabase.java
@@ -1,2 +1,14 @@
-package com.petshopadmin.adapter.output.repository.database.Species;public class SpecieRepositoryDatabase {
+package com.petshopadmin.adapter.output.repository.database.species;
+
+import com.petshopadmin.application.domain.SpecieDomain;
+
+public class SpecieRepositoryDatabase implements com.petshopadmin.application.port.output.database.SpecieRepositoryDatabase {
+   private final SpecieJPARepository specieJPARepository;
+
+   public SpecieRepositoryDatabase (SpecieJPARepository specieJPARepository) {
+       this.specieJPARepository = specieJPARepository;
+   }
+
+    @Override
+    public SpecieDomain getByID(Long id) {return null;}
 }
