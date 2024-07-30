dfe5e3cceba2280c607cdde4599f24456d9bde3c
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-29 23:31:09 -0300

    feat: implement SpecieJPARepository in adapter layer

--- src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieJPARepository.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieJPARepository.java
@@ -1,2 +1,5 @@
-package com.petshopadmin.adapter.output.repository.database.Species;public interface SpecieJPARepository {
-}
+package com.petshopadmin.adapter.output.repository.database.species;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecieJPARepository extends JpaRepository<SpecieDatabase, Long> {}
