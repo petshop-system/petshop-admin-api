0e65eecff6ad74a6e96172dc1a9d0b28354a73c3
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-29 23:29:36 -0300

    feat: implement speciedatabase entity

--- src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieDatabase.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/species/SpecieDatabase.java
@@ -1,2 +1,26 @@
-package com.petshopadmin.adapter.output.repository.database.Species;public class SpecieDatabase {
+package com.petshopadmin.adapter.output.repository.database.species;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "species", schema = "petshop_api")
+@Entity(name = "species")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SpecieDatabase {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @NotNull
+    private long ID;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name is required")
+    private String name;
 }
