7af9ebb65859082ff140fd4db43104b60269b469
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-29 23:35:30 -0300

    feat: implement SpecieService in service layer

--- src/main/java/com/petshopadmin/application/service/SpecieService.java
+++ src/main/java/com/petshopadmin/application/service/SpecieService.java
@@ -1,2 +1,13 @@
-package com.petshopadmin.application.service;public class SpecieService {
+package com.petshopadmin.application.service;
+
+import com.petshopadmin.application.domain.SpecieDomain;
+import com.petshopadmin.application.port.output.database.SpecieRepositoryDatabase;
+
+public class SpecieService {
+    private final SpecieRepositoryDatabase specieRepositoryDatabase;
+
+    public SpecieService(SpecieRepositoryDatabase specieRepositoryDatabase) {
+        this.specieRepositoryDatabase = specieRepositoryDatabase;
+    }
+
 }
