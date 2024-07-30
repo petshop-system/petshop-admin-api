922efa639da34cd48383f5ee1f979e9c6860eab6
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-07-29 23:22:53 -0300

    style: package indentation and organization

--- src/main/java/com/petshopadmin/adapter/output/repository/database/contract/ContractJPARepository.java
+++ src/main/java/com/petshopadmin/adapter/output/repository/database/contract/ContractJPARepository.java
@@ -1,4 +1,4 @@
-package com.petshopadmin.adapter.output.repository.database;
+package com.petshopadmin.adapter.output.repository.database.contract;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
