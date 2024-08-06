33a8ddf9a54fba494a8956702176e8f11a9ad67a
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-06 03:16:43 -0300

    refactor: implement refactoring

--- src/main/java/com/petshopadmin/application/port/input/SpeciesUserCase.java
+++ src/main/java/com/petshopadmin/application/port/input/SpeciesUserCase.java
@@ -1,2 +1,9 @@
-package com.petshopadmin.application.port.input;public interface SpeciesUserCase {
+package com.petshopadmin.application.port.input;
+
+import com.petshopadmin.application.domain.SpecieDomain;
+import com.petshopadmin.exception.InternalServerErrorException;
+import com.petshopadmin.exception.NotFoundException;
+
+public interface SpeciesUserCase {
+    SpecieDomain getByID(Long specieID) throws InternalServerErrorException, NotFoundException;
 }
