485bc63931a6d2f8fafac78302a335cf99b24b14
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-06 03:19:23 -0300

    build: implement species configuration beans

--- src/main/java/com/petshopadmin/configuration/SpecieConfiguration.java
+++ src/main/java/com/petshopadmin/configuration/SpecieConfiguration.java
@@ -1,2 +1,22 @@
-package com.petshopadmin.configuration;public class SpecieConfiguration {
+package com.petshopadmin.configuration;
+
+import com.petshopadmin.adapter.output.repository.database.species.SpecieJPARepository;
+import com.petshopadmin.adapter.output.repository.database.species.SpecieRepositoryDatabase;
+import com.petshopadmin.application.port.input.SpeciesUserCase;
+import com.petshopadmin.application.service.SpecieService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpecieConfiguration {
+
+    @Bean
+    SpecieRepositoryDatabase specieRepositoryDatabase(SpecieJPARepository specieJPARepository) {
+        return new SpecieRepositoryDatabase(specieJPARepository);
+    }
+
+    @Bean
+    SpeciesUserCase speciesUserCase(SpecieRepositoryDatabase specieRepositoryDatabase) {
+        return new SpecieService(specieRepositoryDatabase);
+    }
 }
