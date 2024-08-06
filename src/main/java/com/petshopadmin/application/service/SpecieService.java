6bd45b5158d26d4b503b4b6c547ba8abbffeb7dc
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-06 03:18:03 -0300

    feat: implement searching for species by ID in the service layer

--- src/main/java/com/petshopadmin/application/service/SpecieService.java
+++ src/main/java/com/petshopadmin/application/service/SpecieService.java
@@ -1,13 +1,35 @@
 package com.petshopadmin.application.service;
 
 import com.petshopadmin.application.domain.SpecieDomain;
+import com.petshopadmin.application.port.input.SpeciesUserCase;
 import com.petshopadmin.application.port.output.database.SpecieRepositoryDatabase;
+import com.petshopadmin.exception.InternalServerErrorException;
+import com.petshopadmin.exception.NotFoundException;
+import org.apache.commons.lang3.ObjectUtils;
 
-public class SpecieService {
+public class SpecieService implements SpeciesUserCase {
     private final SpecieRepositoryDatabase specieRepositoryDatabase;
+
+    static String SPECIE_INTERNAL_SERVER_ERROR = "specie internal server error";
+    static String SPECIE_NOT_FOUND = "specie not found";
 
     public SpecieService(SpecieRepositoryDatabase specieRepositoryDatabase) {
         this.specieRepositoryDatabase = specieRepositoryDatabase;
+    }
+
+    @Override
+    public SpecieDomain getByID(Long specieID) throws  InternalServerErrorException, NotFoundException{
+        if (ObjectUtils.isEmpty(specieID)) {
+            throw new InternalServerErrorException(SPECIE_INTERNAL_SERVER_ERROR);
+        }
+
+        SpecieDomain specieDomain = specieRepositoryDatabase.getByID(specieID);
+
+        if (ObjectUtils.isEmpty(specieDomain)) {
+            throw new NotFoundException(SPECIE_NOT_FOUND);
+        }
+
+        return specieDomain;
     }
 
 }
