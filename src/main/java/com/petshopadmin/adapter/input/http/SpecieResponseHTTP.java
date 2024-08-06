60ea163b074743d27ecc457ef66d11b77c5eb031
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-06 03:13:24 -0300

    feat: create DTO response

--- src/main/java/com/petshopadmin/adapter/input/http/SpecieResponseHTTP.java
+++ src/main/java/com/petshopadmin/adapter/input/http/SpecieResponseHTTP.java
@@ -1,2 +1,19 @@
-package com.petshopadmin.adapter.input.http;public class SpecieResponseHTTP {
+package com.petshopadmin.adapter.input.http;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.petshopadmin.application.domain.SpecieDomain;
+import lombok.Data;
+
+@Data
+public class SpecieResponseHTTP {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    SpecieResponseHTTP (SpecieDomain specieDomain) {
+        this.setId(specieDomain.getId());
+        this.setName(specieDomain.getName());
+    }
 }
