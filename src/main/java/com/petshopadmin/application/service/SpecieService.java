f978c48f1f55ee647eb66d2aaaceb3f3859fa0c2
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-10-14 23:20:33 -0300

    feature: create species registry

--- src/main/java/com/petshopadmin/application/service/SpecieService.java
+++ src/main/java/com/petshopadmin/application/service/SpecieService.java
@@ -5,8 +5,13 @@ import com.petshopadmin.application.port.input.SpeciesUserCase;
 import com.petshopadmin.application.port.output.database.SpecieRepositoryDatabase;
 import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
+import com.petshopadmin.exception.ValidationException;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SpecieService implements SpeciesUserCase {
@@ -14,6 +19,8 @@ public class SpecieService implements SpeciesUserCase {
 
     static String SPECIE_INTERNAL_SERVER_ERROR = "species internal server error";
     static String SPECIE_NOT_FOUND = "specie not found";
+    static String ILLEGAL_ARGUMENT_NAME_EXCEPTION = "name cannot be null or empty";
+    static String ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION = "exceeds maximum length of 255 characters";
 
     public SpecieService(SpecieRepositoryDatabase specieRepositoryDatabase) {
         this.specieRepositoryDatabase = specieRepositoryDatabase;
@@ -29,6 +36,35 @@ public class SpecieService implements SpeciesUserCase {
         }
 
         return specieDomain;
+    }
+
+    @Override
+    public SpecieDomain create(SpecieDomain specieDomain) throws InternalServerErrorException, ValidationException {
+        this.validate(specieDomain);
+        return specieRepositoryDatabase.save(specieDomain);
+    }
+
+    @Override
+    public void validate(SpecieDomain specieDomain) throws InternalServerErrorException, ValidationException {
+        if (ObjectUtils.isEmpty(specieDomain)) {
+            throw new InternalServerErrorException(SPECIE_INTERNAL_SERVER_ERROR);
+        }
+
+        List<String> errors = new ArrayList<>();
+
+        specieDomain.setName(StringUtils.trimToEmpty(specieDomain.getName()));
+        if (ObjectUtils.isEmpty(specieDomain.getName())) {
+            errors.add(ILLEGAL_ARGUMENT_NAME_EXCEPTION);
+        }
+
+        if (specieDomain.getName().length() > 255) {
+            errors.add(ILLEGAL_ARGUMENT_CHARACTERS_MAX_NAME_EXCEPTION);
+        }
+
+        if (!errors.isEmpty()) {
+            throw new ValidationException(errors, HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 }
