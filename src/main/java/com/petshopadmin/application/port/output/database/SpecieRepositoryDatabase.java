b60e0c8c7ab0ec94b7b519da214b74620a2c8023
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-10-14 23:20:19 -0300

    feature: create species registry

--- src/main/java/com/petshopadmin/application/port/output/database/SpecieRepositoryDatabase.java
+++ src/main/java/com/petshopadmin/application/port/output/database/SpecieRepositoryDatabase.java
@@ -4,4 +4,5 @@ import com.petshopadmin.application.domain.SpecieDomain;
 
 public interface SpecieRepositoryDatabase {
     SpecieDomain getByID(Long specieID);
+    SpecieDomain save(SpecieDomain specieDomain);
 }
