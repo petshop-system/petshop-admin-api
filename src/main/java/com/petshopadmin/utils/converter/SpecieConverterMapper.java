3401cb81c061b38b8bd6aa298297a2964753bf6f
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-10-14 23:21:11 -0300

    feature: create species converter mapper

--- src/main/java/com/petshopadmin/utils/converter/SpecieConverterMapper.java
+++ src/main/java/com/petshopadmin/utils/converter/SpecieConverterMapper.java
@@ -1,2 +1,13 @@
-package com.petshopadmin.utils.converter;public interface SpecieConverterMapper {
+package com.petshopadmin.utils.converter;
+
+import com.petshopadmin.adapter.input.http.species.SpecieRequestHTTP;
+import com.petshopadmin.application.domain.SpecieDomain;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface SpecieConverterMapper {
+
+    @Mapping(target = "id" , source = "specieid")
+    SpecieDomain toSpecieDomain(SpecieRequestHTTP source);
 }
