90dd231c14b7e9bda92c12e671a59093424ecc7d
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-11-10 20:10:52 -0300

    refactor: include log in the controller layer

--- src/main/java/com/petshopadmin/adapter/input/http/species/SpecieController.java
+++ src/main/java/com/petshopadmin/adapter/input/http/species/SpecieController.java
@@ -7,6 +7,8 @@ import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
 import com.petshopadmin.exception.ValidationException;
 import com.petshopadmin.utils.converter.SpecieConverterMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +17,10 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping(path = "species")
+@RequestMapping(path = "species", produces =  MediaType.APPLICATION_JSON_VALUE)
 public class SpecieController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpecieController.class);
 
     private final SpeciesUserCase speciesUserCase;
     private final SpecieConverterMapper specieConverterMapper;
@@ -29,23 +33,29 @@ public class SpecieController {
 
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/{id}")
     public ResponseHTTP getByID(@PathVariable(name = "id", required = true) Long speciesID)
             throws InternalServerErrorException, NotFoundException
     {
         SpecieDomain specieDomain = speciesUserCase.getByID(speciesID);
 
+        logger.info("Receive request for id: {}", speciesID);
+
         return new ResponseHTTP("success to get species by id", new SpecieResponseHTTP(specieDomain), null, LocalDateTime.now());
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(produces =  MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public ResponseHTTP create(@RequestBody SpecieRequestHTTP specieRequestHTTP)
             throws InternalServerErrorException, ValidationException {
+
+        logger.info("Receive request for create service: {}", specieRequestHTTP);
         try {
             SpecieDomain specieDomain = specieConverterMapper.toSpecieDomain(specieRequestHTTP);
 
             speciesUserCase.create(specieDomain);
+            logger.debug("Response: {}", specieDomain);
+
             return new ResponseHTTP("sucess to create a new species",  new SpecieResponseHTTP(specieDomain), null, LocalDateTime.now());
         } catch (ValidationException e) {
             return new ResponseHTTP("Errors Found", null, Arrays.asList(e.getMessages().toArray()), LocalDateTime.now());
