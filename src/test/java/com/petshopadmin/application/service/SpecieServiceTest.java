7d102bb8be17fd06b74031771351e34761afbdfa
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-11-21 00:29:53 -0300

    test: add test for query by species

--- src/test/java/com/petshopadmin/application/service/SpecieServiceTest.java
+++ src/test/java/com/petshopadmin/application/service/SpecieServiceTest.java
@@ -3,6 +3,7 @@ package com.petshopadmin.application.service;
 import com.petshopadmin.application.domain.SpecieDomain;
 import com.petshopadmin.application.port.input.SpeciesUserCase;
 import com.petshopadmin.application.port.output.database.SpecieRepositoryDatabase;
+import com.petshopadmin.exception.InternalServerErrorException;
 import com.petshopadmin.exception.NotFoundException;
 import com.petshopadmin.exception.ValidationException;
 import org.junit.jupiter.api.Assertions;
@@ -28,6 +29,18 @@ public class SpecieServiceTest {
     }
 
     @Test
+    public void getByIDShouldReturnAny() throws InternalServerErrorException, NotFoundException {
+        SpecieDomain expectedResult = this.getDefaultSpecieDomain();
+
+        Mockito.when(specieRepositoryDatabase.getByID(Mockito.anyLong())).thenReturn(expectedResult);
+
+        SpeciesUserCase userCase = this.getSpecieService();
+        SpecieDomain result = userCase.getByID(1L);
+
+        Assertions.assertEquals(result, expectedResult);
+    }
+
+    @Test
     public void getByIDShouldThrowNotFoundException() {
         NotFoundException ex = Assertions.assertThrows(NotFoundException.class, () -> {
             Mockito.when(specieRepositoryDatabase.getByID(Mockito.any())).thenReturn(null);
@@ -37,6 +50,40 @@ public class SpecieServiceTest {
 
         Assertions.assertEquals(SpecieService.SPECIE_NOT_FOUND, ex.getMessage());
     }
+
+    @Test
+    public void getByNameShouldReturnAny() throws InternalServerErrorException, NotFoundException {
+        SpecieDomain expectedResult = this.getDefaultSpecieDomain();
+
+        Mockito.when(specieRepositoryDatabase.getByName(Mockito.anyString())).thenReturn(expectedResult);
+
+        SpeciesUserCase usercase = this.getSpecieService();
+        SpecieDomain result = usercase.getByName("Felino");
+
+        Assertions.assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void getByNameShouldThrowInternalServerErrorException() {
+        InternalServerErrorException ex = Assertions.assertThrows(InternalServerErrorException.class, () -> {
+            SpeciesUserCase usercase = this.getSpecieService();
+            usercase.getByName(null);
+        });
+        Assertions.assertEquals(SpecieService.SPECIE_INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    @Test
+    public void getByNameShouldThrowNotFoundException() {
+        NotFoundException ex = Assertions.assertThrows(NotFoundException.class, () -> {
+           Mockito.when(specieRepositoryDatabase.getByName(Mockito.anyString())).thenReturn(null);
+
+           SpeciesUserCase usercase = this.getSpecieService();
+           usercase.getByName("SomeName");
+        });
+
+        Assertions.assertEquals(SpecieService.SPECIE_NOT_FOUND, ex.getMessage());
+    }
+
 
     @Test
     public void createSpecieWithTooLongNameShouldThrowValidationException() {
